a402bef6b7d4c7ca04c6554e11a708717d55038f
Sandra Coburn <sandra_coburn@hotmail.com>
2021-02-06 11:30:46 -0800

    Styled Logo and Header

--- components/Header.js
+++ components/Header.js
@@ -1,15 +1,65 @@
 import Link from 'next/link';
+import styled from 'styled-components';
 import Nav from './Nav';
+const theme = {
+  green: ' #c1e5e3',
+  black: '#866f57',
+  grey: '#3A3A3A',
+  lightgrey: '#E1E1E1',
+  offWhite: '#EDEDED',
+  maxWidth: '1000px',
+  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
+};
+
+const Logo = styled.h1`
+  background: #c1e5e3;
+  font-size: 4rem;
+  margin-left: 2rem;
+  position: relative;
+  z-index: 2;
+  transform: skew(-7deg);
+  a {
+    padding: 0.5rem 1rem;
+    color: white;
+    text-transform: uppercase;
+    text-decoration: none;
+  }
+  @media (max-width: 1300px) {
+    margin: 0;
+    text-align: center;
+  }
+`;
+const HeaderStyled = styled.header`
+  .bar {
+    border-bottom: 10px solid var(--black, black);
+    display: grid;
+    grid-template-columns: auto 1fr;
+    justify-content: space-between;
+    align-items: center;
+    @media (max-width: 1300px) {
+      grid-template-columns: 1fr;
+      justify-content: center;
+    }
+  }
+  .sub-bar {
+    display: grid;
+    grid-template-columns: 1fr auto;
+    border-bottom: 10px solid var(--black, black);
+  }
+`;
+
 export default function Header() {
   return (
-    <header>
+    <HeaderStyled>
       <div className="bar">
-        <Link href="/">Midnight Bakery</Link>
+        <Logo>
+          <Link href="/">Midnight Bakery</Link>
+        </Logo>
       </div>
       <div className="sub-bar">
         <p>Search</p>
       </div>
       <Nav />
-    </header>
+    </HeaderStyled>
   );
 }
