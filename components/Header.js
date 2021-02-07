570fd9a82e53d5d7944f2eed0e314ff221c30975
Sandra Coburn <sandra_coburn@hotmail.com>
2021-02-06 18:24:40 -0800

    Added progress bar to header

--- components/Header.js
+++ components/Header.js
@@ -1,19 +1,10 @@
 import Link from 'next/link';
 import styled from 'styled-components';
 import Nav from './Nav';
-const theme = {
-  green: ' #c1e5e3',
-  black: '#866f57',
-  grey: '#3A3A3A',
-  lightgrey: '#E1E1E1',
-  offWhite: '#EDEDED',
-  maxWidth: '1000px',
-  bs: '0 12px 24px 0 rgba(0, 0, 0, 0.09)',
-};
 
 const Logo = styled.h1`
   background: #c1e5e3;
-  font-size: 4rem;
+  font-size: 2rem;
   margin-left: 2rem;
   position: relative;
   z-index: 2;
@@ -44,7 +35,7 @@ const HeaderStyled = styled.header`
   .sub-bar {
     display: grid;
     grid-template-columns: 1fr auto;
-    border-bottom: 10px solid var(--black, black);
+    border-bottom: 2px solid var(--lightgrey, lightgrey);
   }
 `;
 
